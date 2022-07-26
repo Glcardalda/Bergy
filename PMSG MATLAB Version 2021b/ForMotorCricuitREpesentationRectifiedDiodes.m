ea4bf9da1072bb525d17f270e92e5133beb816fd
Glcardalda <106836785+Glcardalda@users.noreply.github.com>
2022-07-26 12:52:50 -0600

    Add files via upload

---
+++ PMSG MATLAB Version 2021b/ForMotorCricuitREpesentationRectifiedDiodes.m
@@ -0,0 +1,15 @@
+
+clc all
+clear all
+%%%%%%%%%%%% Voltage Source Parameters%%%%%%%%%%%%%%%%
+f=40;
+Vg=f*32*9*0.0341895;
+% c=250e-9;
+% P=20348.62314;
+% N=140;
+% Tm=-P/(2*pi*N/60);
+%%%%%%%%%%%%Parameters for Passive Rectifier%%%%%%%%%%%%
+Cd=0;
+Rd=1e6;
+RL=20;
+Windspeed=14;       % Units m/s
